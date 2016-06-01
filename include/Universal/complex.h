10fe65534f1cba879ffb8d152f6524a886af921c
centurycoder <541767950@qq.com>
2016-06-01 11:51:48 +0800

    update complex.h Note

--- include/Universal/complex.h
+++ include/Universal/complex.h
@@ -9,22 +9,55 @@
 class Complex
 {
 private:
+	/** 复数实部 
+	  */
     double m_real;
+	/** 复数虚部
+	  */
     double m_imag;
 public:
-    Complex();
-    Complex(const char* str);
+    /** 构造默认复数0 
+	  */
+	Complex();
+    /** 从字符串“(a+bj)"或"a"中获取a,b, 以a为实部，b为虚部
+      * ,构造复数
+      */
+	Complex(const char* str);
+	/** 输入实部real，虚部imag构造复数
+      */
     Complex(double real, double imag);
+	/** 复数生成字符串，产生格式："(a+/-bj)",纯实数不带括号
+	  */
     std::string ToString();
+	/** 返回实部
+      */
     double real(){ return m_real;}
+	/** 返回虚部
+      */
     double imag(){ return m_imag;}
+	/** 修改实部
+	  */
     void real(double real){ m_real = real;}
+	/** 修改虚部
+	  */ 
     void imag(double imag){ m_imag = imag;}
+	/** 复数求模
+	  */
     double modulus();
+	/** 复数求模平方
+      */
     double squareMod();
+	/** 复数加法
+	  */
     Complex operator+(Complex x);
+	/** 复数减法
+	  */
     Complex operator-(Complex x);
+	/** 复数乘法
+      */
     Complex operator*(Complex x);
+	/** 复数除法
+	  */
     Complex operator/(Complex x);
 };
 
